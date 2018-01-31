13d6fb392ce63ce0b18595b50e62ef51d51c2a30
weikoo <35066175+weikoo@users.noreply.github.com>
2018-02-01 02:34:52 +0800

    阿萨德
    大润发

---
+++ class2/用Python设计第一个游戏.py
@@ -0,0 +1,13 @@
+temp=input("猜数字")
+#input返回字符串
+guess=int(temp)
+if guess==8:
+	print("答对了!")
+elif guess==10:
+	print("差一点!")
+else:
+        print("打错了！")
+print("不玩了")
+
+temp=input("请输入你的姓名:")
+print("你好"+temp)
